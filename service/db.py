9c81ab938229ee5de2dc3fddd4da14ac51392f09
Andre Wagner <andre.nho@gmail.com>
2022-01-12 15:52:21 -0300

    .

--- service/db.py
+++ service/db.py
@@ -5,3 +5,25 @@ class DB:
 
     def __init__(self):
         self.con = sqlite3.connect(os.environ['DB_DIR'] + '/kindlr.db')
+        self.con.cursor().execute('''
+            CREATE TABLE IF NOT EXISTS jobs (
+                id              TEXT PRIMARY KEY,
+                file            TEXT NOT NULL,
+                skip_conversion INTEGER TEXT NOT NULL,
+                step            TEXT,
+                state           TEXT,
+                message         TEXT
+            );
+        ''')
+
+    def set_step(self, job, step, state, message=None):
+        cur = self.con.cursor()
+        cur.begin()
+        cur.execute('''
+            UPDATE jobs
+               SET step = ?
+                 , state = ?
+                 , message = ?
+             WHERE id = ?''', (step, state, job, message))
+        cur.commit()
+        cur.close()
