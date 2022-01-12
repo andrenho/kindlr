2ebac62ebf49be4ae13f6dde6e1c929f0c4ff612
Andre Wagner <andre.nho@gmail.com>
2022-01-12 17:45:02 -0300

    .

--- service/__main__.py
+++ service/__main__.py
@@ -1,4 +1,15 @@
+import time
+import threading
+
 from db import DB
+
+def execute_job(db, job):
+    print('Executing job ' + job)
 
 if __name__ == '__main__':
     db = DB()
+    while True:
+        for job in db.reserve_pending_jobs():
+            print(job)
+            threading.Thread(target=execute_job, args=(db,job)).start()
+        time.sleep(0.2)
--- service/db.py
+++ service/db.py
@@ -1,29 +1,42 @@
 import os
 import sqlite3
 
+class Job:
+    def __init__(self, id, file, skip_conversion):
+        self.id = id
+        self.file = file
+        self.skip_conversion = skip_conversion
+
 class DB:
 
     def __init__(self):
         self.con = sqlite3.connect(os.environ['DB_DIR'] + '/kindlr.db')
-        self.con.cursor().execute('''
+        cur = self.con.cursor()
+        cur.execute('''
             CREATE TABLE IF NOT EXISTS jobs (
                 id              TEXT PRIMARY KEY,
                 file            TEXT NOT NULL,
                 skip_conversion INTEGER TEXT NOT NULL,
                 step            TEXT,
                 state           TEXT,
-                message         TEXT
+                message         TEXT,
+                time            TEXT
             );
         ''')
 
+    def reserve_pending_jobs(self):
+        cur = self.con.cursor()
+        rows = cur.execute('SELECT id, file, skip_conversion FROM jobs WHERE step IS NULL ORDER BY time').fetchall()
+        cur.execute("UPDATE jobs SET step = 'reserved' WHERE step IS NULL")
+        self.con.commit()
+        return map(lambda r: Job(r[0], r[1], r[2]), rows)
+
     def set_step(self, job, step, state, message=None):
         cur = self.con.cursor()
-        cur.begin()
         cur.execute('''
             UPDATE jobs
                SET step = ?
                  , state = ?
                  , message = ?
              WHERE id = ?''', (step, state, job, message))
-        cur.commit()
-        cur.close()
+        self.con.commit()
